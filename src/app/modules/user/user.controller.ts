a19adc005b42581f57a47a56e90e8aad1b14506c
mdrasel97 <mollarasel972@gmail.com>
2026-01-28 20:56:33 +0600

    user validation added

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { IUser } from "./user.interface";
+import { UserService } from "./user.service";
+
+const getCurrentUser = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.getCurrentUser(req);
+  sendResponse<IUser>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Current user retrieved successfully",
+    data: result,
+  });
+});
+
+const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.getAllUsers();
+  sendResponse<IUser[]>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "All users retrieved successfully",
+    data: result,
+  });
+});
+
+const updateUserStatus = catchAsync(async (req: Request, res: Response) => {
+  const userId = req.params.id as string;
+  const newStatus = req.body.status;
+
+  const result = await UserService.updateUserStatus(userId, newStatus);
+  sendResponse<IUser>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "User status updated successfully",
+    data: result,
+  });
+});
+
+export const UserController = {
+  getCurrentUser,
+  getAllUsers,
+  updateUserStatus,
+};
---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { UserRole } from "../../../../generated/prisma/enums";
+import authMiddleware from "../../middleware/Auth";
+import { UserController } from "./user.controller";
+
+const router = express.Router();
+
+router.get(
+  "/me",
+  authMiddleware(UserRole.admin, UserRole.customer, UserRole.provider),
+  UserController.getCurrentUser,
+);
+
+router.get("/", authMiddleware(UserRole.admin), UserController.getAllUsers);
+
+router.patch(
+  "/:id",
+  authMiddleware(UserRole.admin),
+  UserController.updateUserStatus,
+);
+
+export const UserRoutes = router;
